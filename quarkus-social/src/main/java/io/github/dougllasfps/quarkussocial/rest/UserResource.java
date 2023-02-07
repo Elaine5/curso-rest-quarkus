366cf79a1eeef55c3825b915fb97b2b174145b25
Elaine5 <lanne9873@gmail.com>
2023-02-07 13:42:54 -0300

    Att

--- quarkus-social/src/main/java/io/github/dougllasfps/quarkussocial/rest/UserResource.java
+++ quarkus-social/src/main/java/io/github/dougllasfps/quarkussocial/rest/UserResource.java
@@ -1,14 +1,23 @@
 package io.github.dougllasfps.quarkussocial.rest;
 
+
+import io.github.dougllasfps.quarkussocial.rest.dto.CreateUserRequest;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
 
     @POST
-    public Response createUser(CreateUserRequest userRequest ){
+    public Response createUser( CreateUserRequest userRequest ){
 
+        return Response.ok(userRequest).build();
     }
 }
