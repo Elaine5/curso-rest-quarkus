15569288fb804999bf9f16ae9ae89020b36be280
Elaine5 <lanne9873@gmail.com>
2023-08-22 14:20:24 -0300

    criações de métodos de postagens

--- quarkus-social/src/main/java/io/github/dougllasfps/quarkussocial/rest/PostResource.java
+++ quarkus-social/src/main/java/io/github/dougllasfps/quarkussocial/rest/PostResource.java
@@ -1,6 +1,10 @@
 package io.github.dougllasfps.quarkussocial.rest;
 
+import io.github.dougllasfps.quarkussocial.domain.model.User;
+import io.github.dougllasfps.quarkussocial.domain.repository.UserRepository;
+
 import javax.annotation.Resource;
+import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -10,14 +14,28 @@ import javax.ws.rs.core.Response;
 @Produces(MediaType.APPLICATION_JSON)
 public class PostResource {
 
-    @POST
-    public Response savePost(){
+    private UserRepository userRepository;
 
+    @Inject
+    public PostResource(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @POST
+    public Response savePost( @PathParam("userId") Long userId ){
+        User user = userRepository.findById(userId);
+        if (user == null){
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
         return Response.status(Response.Status.CREATED).build();
     }
 
     @GET
-    public Response listPosts(){
+    public Response listPosts( @PathParam("userId") Long userId ){
+        User user = userRepository.findById(userId);
+        if (user == null){
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
 
         return Response.ok().build();
     }
