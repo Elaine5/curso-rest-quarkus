e7ed77fb551b27e5b160ae98cc001733b0778aff
Elaine5 <lanne9873@gmail.com>
2023-08-22 14:47:02 -0300

    criação da entity posts

---
+++ quarkus-social/src/main/java/io/github/dougllasfps/quarkussocial/domain/model/Post.java
@@ -0,0 +1,27 @@
+package io.github.dougllasfps.quarkussocial.domain.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "posts")
+@Data
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "post_text")
+    private String text;
+
+    @Column(name = "dateTime")
+    private LocalDateTime dataTime;
+
+    @ManyToOne //por ter chave estrangeira, quer dizer também que teremos muitas postagens para um unico usuário
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
