ad2281503763a7fdd9745ffa6b51bfd14e9ea42a
Elaine5 <lanne9873@gmail.com>
2023-09-12 16:03:44 -0300

    atualização projeto, criação do request e resource

--- quarkus-social/src/main/java/io/github/dougllasfps/quarkussocial/rest/resource/FollowerResource.java
+++ quarkus-social/src/main/java/io/github/dougllasfps/quarkussocial/rest/resource/FollowerResource.java
@@ -1,21 +1,16 @@
 package io.github.dougllasfps.quarkussocial.rest.resource;
 
-import io.github.dougllasfps.quarkussocial.domain.model.Post;
-import io.github.dougllasfps.quarkussocial.domain.model.User;
+import io.github.dougllasfps.quarkussocial.domain.model.Follower;
 import io.github.dougllasfps.quarkussocial.domain.repository.FollowerRepository;
 import io.github.dougllasfps.quarkussocial.domain.repository.UserRepository;
-import io.github.dougllasfps.quarkussocial.rest.dto.CreatePostRequest;
-import io.github.dougllasfps.quarkussocial.rest.dto.PostResponse;
-import io.quarkus.panache.common.Sort;
+import io.github.dougllasfps.quarkussocial.rest.dto.FollowerRequest;
 
 import javax.inject.Inject;
-import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.stream.Collectors;
 
-@Path("/users/{userId/followers}")
+@Path("/users/{userId}/followers")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class FollowerResource {
@@ -30,46 +25,24 @@ public class FollowerResource {
         this.userRepository = userRepository;
     }
 
+    @PUT
+    public Response followUser(@PathParam("userId") Long userId, FollowerRequest request) {
 
+        var user = userRepository.findById(userId);
+        if (user == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
 
-//    @POST
-//    @Transactional // para fazer alterção na base é necessario esta anotação
-//    public Response savePost(
-//            @PathParam("userId") Long userId, CreatePostRequest request){
-//
-//        User user = userRepository.findById(userId);
-//        if (user == null){
-//            return Response.status(Response.Status.NOT_FOUND).build();
-//        }
-//
-//        Post post = new Post();
-//        post.setText(request.getText());
-//        post.setUser(user);
-//
-//        repository.persist(post);
-//
-//        return Response.status(Response.Status.CREATED).build();
-//    }
+        var follower = userRepository.findById(request.getFollowerId());
 
-//    @GET
-//    public Response listPosts( @PathParam("userId") Long userId ){
-//        User user = userRepository.findById(userId);
-//        if (user == null){
-//            return Response.status(Response.Status.NOT_FOUND).build();
-//        }
-//
-//        var query = repository.find("user", Sort.by("dateTime",
-//                Sort.Direction.Descending), user);
-//        var list = query.list();
-//
-//        // está forma de código foi escrita para ao listar os dados, apenas venha os dados do text e do dateTime
-//        var postResponseList = list.stream()
-//                //.map(post -> PostResponse.fromEntity(post)) // segunda forma que pode ser escrita
-//                .map(PostResponse::fromEntity) // forma escrita significa método de referencia
-//                .collect(Collectors.toList());
-//
-//        return Response.ok(postResponseList).build();
-//    }
+        var entity = new Follower();
+        entity.setUser(user);
+        entity.setFollower(follower);
+
+        repository.persist(entity);
+
+        return Response.status(Response.Status.NO_CONTENT).build();
+    }
 
 }
 
