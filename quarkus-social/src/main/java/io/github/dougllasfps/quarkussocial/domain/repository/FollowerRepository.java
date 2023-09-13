ca7575ff2c4bd1816e49aef3faa84dc64403c9d3
Elaine5 <lanne9873@gmail.com>
2023-09-13 12:26:29 -0300

    ajustes

--- quarkus-social/src/main/java/io/github/dougllasfps/quarkussocial/domain/repository/FollowerRepository.java
+++ quarkus-social/src/main/java/io/github/dougllasfps/quarkussocial/domain/repository/FollowerRepository.java
@@ -1,10 +1,15 @@
 package io.github.dougllasfps.quarkussocial.domain.repository;
 
 import io.github.dougllasfps.quarkussocial.domain.model.Follower;
+import io.github.dougllasfps.quarkussocial.domain.model.User;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
 public class FollowerRepository implements PanacheRepository<Follower> {
 
+    public boolean follows(User follower, User user){
 
-
+    }
 }
--- quarkus-social/src/main/java/io/github/dougllasfps/quarkussocial/rest/resource/FollowerResource.java
+++ quarkus-social/src/main/java/io/github/dougllasfps/quarkussocial/rest/resource/FollowerResource.java
@@ -6,6 +6,7 @@ import io.github.dougllasfps.quarkussocial.domain.repository.UserRepository;
 import io.github.dougllasfps.quarkussocial.rest.dto.FollowerRequest;
 
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -26,6 +27,7 @@ public class FollowerResource {
     }
 
     @PUT
+    @Transactional
     public Response followUser(@PathParam("userId") Long userId, FollowerRequest request) {
 
         var user = userRepository.findById(userId);
@@ -34,6 +36,8 @@ public class FollowerResource {
         }
 
         var follower = userRepository.findById(request.getFollowerId());
+
+//        boolean follows = repository.follows(follower, user);
 
         var entity = new Follower();
         entity.setUser(user);
